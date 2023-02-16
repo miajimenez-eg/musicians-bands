52ae1d711ce5dffa836aa28e1cdd9751b3ced08e
Mia Jimenez <miajimenez@expediagroup.com>
2023-02-16 12:17:17 +0000

    undo changes

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const {Band} = require('./Band')
 const {Musician} = require('./Musician')
-const {Song} = require('./Song')
 
 Band.hasMany(Musician);
-Band.belongsToMany(Song)
-Song.belongsToMany(Band)
 Musician.belongsTo(Band);
 
 module.exports = {
